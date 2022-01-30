cfbc7890ce95c361ce8e94cdd4587bcd820355d9
ilyatrushkov <90609738+ilyatrushkov@users.noreply.github.com>
2022-01-30 18:17:39 +0300

    Update StackItem.hpp

--- include/StackItem.hpp
+++ include/StackItem.hpp
@@ -1,7 +1,7 @@
 // Copyright 2022 Trushkov Ilya ilya.tr20002@gmail.com
 
-#ifndef INCLUDE_STACKITEM_HPP
-#define INCLUDE_STACKITEM_HPP
+#ifndef INCLUDE_STACKITEM_HPP_
+#define INCLUDE_STACKITEM_HPP_
 
 template <typename T>
 struct  Item {
@@ -9,4 +9,4 @@ struct  Item {
   Item<T> *previous;
 };
 
-#endif  // INCLUDE_STACKITEM_HPP
+#endif  // INCLUDE_STACKITEM_HPP_
