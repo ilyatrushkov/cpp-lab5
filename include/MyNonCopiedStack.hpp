ddb794f4b4e9708355322fb452985308f8e0dec9
ilyatrushkov <90609738+ilyatrushkov@users.noreply.github.com>
2022-01-31 12:34:29 +0300

    Update MyNonCopiedStack.hpp

--- include/MyNonCopiedStack.hpp
+++ include/MyNonCopiedStack.hpp
@@ -26,13 +26,13 @@ class MyNonCopiedStack {
 
 template <typename T>
 template <typename ... Args>
-void MyNonCopiedStack<T>::push_emplace(Args&& ... value) {
+inline void MyNonCopiedStack<T>::push_emplace(Args&& ... value) {
   auto *tmp = _top_item;
   _top_item = new Item<T>{{std::forward<Args>(value) ... }, tmp};
 }
 
 template <typename T>
-void MyNonCopiedStack<T>::push(T &&value) {
+inline void MyNonCopiedStack<T>::push(T &&value) {
   auto *tmp = _top_item;
   _top_item = new Item<T>{std::forward<T>(value), tmp};
 }
